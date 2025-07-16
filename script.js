b2d66dd1b84ad8ccf915a70b6bde5aaa6eff196a
clrnlvly <142940454+clrnlvly@users.noreply.github.com>
2025-07-16 18:51:36 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const passwordInput = document.getElementById("password");
+const lengthSlider = document.getElementById("length");
+const lengthDisplay = document.getElementById("length-value");
+const uppercaseCheckbox = document.getElementById("uppercase");
+const lowercaseCheckbox = document.getElementById("lowercase");
+const numbersCheckbox = document.getElementById("numbers");
+const symbolsCheckbox = document.getElementById("symbols");
+const generateButton = document.getElementById("generate-btn");
+const copyButton = document.getElementById("copy-btn");
+const strengthBar = document.querySelector(".strength-bar");
+const strengthText = document.getElementById(".strength-container p");
+
+// Character sets
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const numberCharacters = "0123456789";
+const symbolCharacters = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+lengthSlider.addEventListener("input", () => {
+   lengthDisplay.textContent = lengthSlider.value; 
+});
+
